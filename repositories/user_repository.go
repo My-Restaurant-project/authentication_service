e8e12c7dfcc07350aed4b57b59ceb9be89f018f1
muhammad <muhammmadazizekubov@gmail.com>
2024-07-08 21:13:53 +0500

    1-commit

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -2,7 +2,9 @@ package repositories
 
 import (
 	user "github.com/Projects/Restaurant_Reservation_System/authentication_service/genproto/authentication_service"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserRepository struct {
@@ -15,6 +17,7 @@ func NewUserRepository(db *sqlx.DB) *UserRepository {
 
 func (r *UserRepository) Login(userReq *user.LoginRequest) (*user.LoginResponse, error) {
 	return nil, nil
+
 }
 
 func (r *UserRepository) GetProfileById(userReq *user.UserIdRequest) (*user.UserIdResponse, error) {
@@ -22,5 +25,32 @@ func (r *UserRepository) GetProfileById(userReq *user.UserIdRequest) (*user.User
 }
 
 func (r *UserRepository) Register(userReq *user.RegisterRequest) (*user.RegisterResponse, error) {
-	return nil, nil
+	query := `INSERT INTO users (id, username, password, email)values($1, $2, $3, $4)`
+
+	password, err := HashPassword(userReq.Profile.Password)
+	if err != nil {
+		return nil, err
+	}
+	id := uuid.NewString()
+	userReq.Profile.Id = id
+
+	userReq.Profile.Password = string(password)
+
+	_, err = r.db.Exec(query, userReq.Profile.Id, userReq.Profile.Name, userReq.Profile.Password, userReq.Profile.Email)
+	if err != nil {
+		return nil, err
+	}
+	profile := &user.Profile{
+		Id:        userReq.Profile.Id,
+		Name:      userReq.Profile.Name,
+		Email:     userReq.Profile.Email,
+		Password:  userReq.Profile.Password,
+		CreatedAt: userReq.Profile.CreatedAt,
+		UpdatedAt: userReq.Profile.UpdatedAt,
+	}
+	return &user.RegisterResponse{Profile: profile}, nil
+}
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
